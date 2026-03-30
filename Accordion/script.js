d01148e3c7723b74cdd732a0aace2289093f3711
Antara Chanda <160458895+meantaraa@users.noreply.github.com>
2026-03-30 07:21:57 +0530

    Accordion

---
+++ Accordion/script.js
@@ -0,0 +1,17 @@
+const buttons = document.querySelectorAll('.accordion-btn');
+
+buttons.forEach((btn) => {
+
+    btn.addEventListener("click", function ()  {
+
+        this.classList.toggle("active");
+
+        const panel = this.nextElementSibling;
+
+        if(panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+});
